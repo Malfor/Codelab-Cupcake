f57023f3ca25dadb268a9b963a1b510cb048c63b
Rodolfo Barrios <rodolfo.barrios@avista.co>
2021-02-25 23:08:30 -0500

    Se implementa el sharedViewModel en fragmento y layout por medio de databinding

--- app/src/main/java/com/example/cupcake/FlavorFragment.kt
+++ app/src/main/java/com/example/cupcake/FlavorFragment.kt
@@ -21,14 +21,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.cupcake.databinding.FragmentFlavorBinding
+import com.example.cupcake.model.OrderViewModel
 
 /**
  * [FlavorFragment] allows a user to choose a cupcake flavor for the order.
  */
 class FlavorFragment : Fragment() {
 
+    private val sharedViewModel: OrderViewModel by activityViewModels()
     // Binding object instance corresponding to the fragment_flavor.xml layout
     // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
     // when the view hierarchy is attached to the fragment.
@@ -45,8 +48,10 @@ class FlavorFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        activity?.title  = getString(R.string.choose_flavor)
         binding?.apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = sharedViewModel
             nextButton.setOnClickListener { goToNextScreen() }
         }
     }
