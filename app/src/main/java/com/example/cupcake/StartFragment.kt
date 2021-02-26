42ff86eb164fced026c2c343abb3777b1f78b013
Rodolfo Barrios <rodolfo.barrios@avista.co>
2021-02-25 23:26:49 -0500

    Se agrega el titulo para la vista principal

--- app/src/main/java/com/example/cupcake/StartFragment.kt
+++ app/src/main/java/com/example/cupcake/StartFragment.kt
@@ -48,6 +48,7 @@ class StartFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        activity?.title = getString(R.string.app_name)
         binding?.startFragment = this
     }
 
