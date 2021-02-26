78640ff9f0c12e6e08ccd06465978944809587a1
Rodolfo Barrios <rodolfo.barrios@avista.co>
2021-02-25 23:09:09 -0500

    Se implementa el sharedViewModel al framgneto y layout por medio de data binding

--- app/src/main/java/com/example/cupcake/PickupFragment.kt
+++ app/src/main/java/com/example/cupcake/PickupFragment.kt
@@ -21,14 +21,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.cupcake.databinding.FragmentPickupBinding
+import com.example.cupcake.model.OrderViewModel
 
 /**
  * [PickupFragment] allows the user to choose a pickup date for the cupcake order.
  */
 class PickupFragment : Fragment() {
 
+    private val sharedViewModel: OrderViewModel by activityViewModels()
     // Binding object instance corresponding to the fragment_pickup.xml layout
     // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
     // when the view hierarchy is attached to the fragment.
@@ -45,8 +48,10 @@ class PickupFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        activity?.title = getString(R.string.choose_pickup_date)
         binding?.apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = sharedViewModel
             nextButton.setOnClickListener { goToNextScreen() }
         }
     }
